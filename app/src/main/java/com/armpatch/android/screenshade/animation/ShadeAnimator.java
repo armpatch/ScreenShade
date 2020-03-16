adf5579f92e215dfc8e1b80b1d5577e1cd941086
Aaron <ampatch@gmail.com>
2020-03-16 12:42:11 -0400

    removed shrinking circle on hide animation, replaced it with a whole screen fade in

--- app/src/main/java/com/armpatch/android/screenshade/animation/ShadeAnimator.java
+++ app/src/main/java/com/armpatch/android/screenshade/animation/ShadeAnimator.java
@@ -13,7 +13,7 @@ public class ShadeAnimator {
     private static final int HIDE_DURATION = 400;
 
     private static final float MIN_SIZE = 0f;
-    private static final float MIN_ALPHA = .6f;
+    private static final float MIN_ALPHA = 0.6f;
 
     public static ObjectAnimator getRevealAnimatorSet(View shadeView) {
         PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, MIN_SIZE, 1f);
@@ -32,12 +32,10 @@ public class ShadeAnimator {
     }
 
     public static ObjectAnimator getHideAnimator(View shadeView) {
-        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, MIN_SIZE);
-        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, MIN_SIZE);
-        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 1f, MIN_ALPHA);
+        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 1f, 0.0f);
 
         ObjectAnimator animator =
-                ObjectAnimator.ofPropertyValuesHolder(shadeView, scaleX, scaleY, alpha);
+                ObjectAnimator.ofPropertyValuesHolder(shadeView, alpha);
 
         BaseInterpolator interpolator = new DecelerateInterpolator();
 
